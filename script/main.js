1253753987156c8fe708ce438c9fd2bdaa8ebeed
ammarmohmed-1 <ammarmlmero@gmail.com>
2023-05-26 12:32:42 +0300

    modify in design

--- script/main.js
+++ script/main.js
@@ -12,9 +12,8 @@ bar.addEventListener("click",()=>{
   bar.innerHTML = botton[A++];
   if(A >= botton.length ){ A=0 }
 
-  nav.classList.toggle("navbar")
+  nav.classList.toggle("navbar");
 })
-
 
 
 // contact us FROM
@@ -29,10 +28,41 @@ start_now.addEventListener("click",()=>{
 });
 
 icon_form.addEventListener("click",()=>{
-  form.classList.add("form_close")
+  form.classList.add("form_close");
 });
 
 
 window.onscroll = ()=>{
   form.classList.add("form_close");
+  nav.classList.remove("navbar");
+  bar.innerHTML = botton[1];
+
 }
+
+// make a auto typing
+
+// heading
+var make_idea = document.querySelector("#make_idea");
+var tittle = "MAKE IDEA";
+let i = 0;
+const make_ideaa = () => {
+  make_idea.innerText = tittle.slice(0,i++);
+  if (tittle.length < i) {
+    clearInterval(stopfun);
+  }
+}
+const stopfun = setInterval(make_ideaa, 200);
+
+// tittle heading 
+
+var heading_MI = document.querySelector(".heading_MI");
+console.log(heading_MI);
+var words = [` انت لديك الفكرة ونحن علينا<span data-aos="zoom-out-down"> التخطيط</span>`,` انت لديك الفكرة ونحن علينا<span data-aos="zoom-out-down"> التصميم الابداعى</span>`,` انت لديك الفكرة ونحن علينا<span data-aos="zoom-out-down"> التنفيذ</span>`];
+let ii =0;
+const heading_MII = () => {
+  heading_MI.innerHTML = words[ii++];
+  if(words.length == ii){
+    ii = 0;
+  }
+}
+setInterval(heading_MII , 2000);
